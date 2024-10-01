7327c6fb70c8d4114084c74422125dd8c81fea0f
e1b22065 <e1b22065@oit.ac.jp>
2024-10-01 14:31:00 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is7/enshu/janken_7/Janken7Application.java
@@ -0,0 +1,13 @@
+package oit.is7.enshu.janken_7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Janken7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Janken7Application.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is7/enshu/janken_7/Janken7ApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is7.enshu.janken_7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Janken7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
